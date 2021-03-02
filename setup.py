ec2309f35a014ecf17720eae2db880f9e9406114
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2021-03-02 16:54:56 -0600

    Version bump to 0.2 and updated report to lp-to-jira.report

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="LpToJira",
-    version="0.1",
+    version="0.2",
     author="Matthieu Clemenceau",
     author_email="matthieu.clemenceau@canonical.com",
     description=("A Command Line helper to import launchpad bug in JIRA."),
