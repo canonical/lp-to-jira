baea62c5804a7fefd0f5b2a165a9e000da54c2df
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2021-08-31 16:05:13 -0500

    Added test for is_bug_in_jira

--- tests/test_lp_to_jira.py
+++ tests/test_lp_to_jira.py
@@ -3,7 +3,10 @@ import pytest
 from unittest.mock import Mock
 
 from LpToJira.lp_to_jira import\
-    get_lp_bug, get_lp_bug_pkg, get_all_lp_project_bug_tasks
+    get_lp_bug,\
+    get_lp_bug_pkg,\
+    get_all_lp_project_bug_tasks,\
+    is_bug_in_jira
 
 
 
@@ -48,10 +51,10 @@ def test_get_lp_bug_pkg():
 
 
 def test_get_lp_project_bug_tasks(lp):
-# Very light testing of what the function could do
-# 100% coverage but not necessarly 100% coverage :)
-# TODO: test more date's options
-# TODO: test various status filters
+    # Very light testing of what the function could do
+    # 100% coverage but not necessarly 100% coverage :)
+    # TODO: test more date's options
+    # TODO: test various status filters
 
     assert get_all_lp_project_bug_tasks(lp, "badproject") == None
 
@@ -60,5 +63,23 @@ def test_get_lp_project_bug_tasks(lp):
 
     # project curtin exists and has a bug
     assert get_all_lp_project_bug_tasks(lp, "curtin", 5).id == 123456
+
+def test_is_bug_in_jira():
+    jira = Mock()
+    jira.search_issues = Mock(return_value=None)
+
+    bug = Mock()
+    bug.id = 123
+
+    assert is_bug_in_jira(jira, bug, "AA") == False
+
+    jira_issue = Mock()
+    jira_issue = [Mock(key="key")]
+
+    jira.search_issues = Mock(return_value=jira_issue)
+    jira.client_info = Mock(return_value="jira_client_info")
+
+    assert is_bug_in_jira(jira, bug, "AA") == True
+
 
 # =============================================================================
